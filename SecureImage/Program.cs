acbc21c2ab27fb76b958e9889159cc1cf3871ec6
Bentley, Matt (UK - London) <matthew_j_bentley@hotmail.com>
2018-12-30 14:02:08 +0000

    initial

---
+++ SecureImage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace SecureImage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            int i = 1;
+            while(true)
+            {
+                Console.WriteLine($"Starting cycle {i}");
+                Thread.Sleep(1000);
+                Console.WriteLine($"Finished cycle {i}");
+                i++;
+            }
+        }
+    }
+}
